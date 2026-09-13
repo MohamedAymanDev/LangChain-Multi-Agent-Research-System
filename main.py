24f657d603dd87134d1a98ee2260baee952c20df
Mohamed_Ayman <mohamed.ayman.pro433@gmail.com>
2026-09-13 18:30:50 +0300

    add pipeline to main

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from src.Tools.tools import web_search, scarp_url
-from src.Agents.agents import llm , build_search_agent , build_reader_agent
+from src.piplines.piplines import run_serch_pipeline
 
-# Build agent 
-search_agent = build_search_agent()
-reader_agent = build_reader_agent()
-
-# test 
-search_res = search_agent.invoke({
-    'messages':[
-        {
-            'role':'user',
-            'content':'Find information abot GPT 6'
-        }
-    ]
-})
-
-# print(search_res)
-reder_res = reader_agent.invoke({
-    'messages':[
-        {
-            'role' : 'user',
-            'content':'Read this URL and Extract its main content: https://fastapi.tiangolo.com/'
-        }
-    ]
-})
-print(reder_res)
+topic = "The impact of AI on the job market in 2026"
+run_serch_pipeline(topic)
