e443910ec7aa03019710051bf4ed06f1a88ff224
Mohamed_Ayman <mohamed.ayman.pro433@gmail.com>
2026-09-13 16:04:27 +0300

    add scrap tool and check it scusseflly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from src.Tools.tools import web_search
+from src.Tools.tools import web_search, scarp_url
 
-res = web_search.invoke("Find Roadmap of Frontend")
+# res = web_search.invoke("Find Roadmap of Frontend")
+# print(res)
+
+url = "https://en.wikipedia.org/wiki/Artificial_intelligence"
+res = scarp_url.invoke(url)
 print(res)
